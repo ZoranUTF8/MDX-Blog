0f544d6a445eac7ae22a84e2a654a4da91d0f9cb
ZoranUTF8 <janjiczoran23@gmail.com>
2022-01-02 11:01:20 +0900

    Add netlify form

--- src/pages/404.js
+++ src/pages/404.js
@@ -2,6 +2,19 @@ import React from 'react'
 import { Link } from 'gatsby'
 import Layout from '../components/Layout'
 
-const NotFoundPage = () => <h2>error page</h2>
-
+const NotFoundPage = () => {
+  return (
+    <Layout>
+      <section className="error-page">
+        <div className="page-center">
+          <span>404</span>
+          <p>Sorry, the page cannot be found...</p>
+          <Link to="/" className="btn">
+            Home
+          </Link>
+        </div>
+      </section>
+    </Layout>
+  )
+}
 export default NotFoundPage
--- src/pages/newsletter.js
+++ src/pages/newsletter.js
@@ -2,7 +2,48 @@ import React from 'react'
 import Layout from '../components/Layout'
 const NewsLetter = () => {
   return (
-    <h4>newsletter</h4>
+    <Layout>
+      <section className="newsletter-page">
+        <div className="page-center">
+          <h2>Get all the latest stories to your inbox</h2>
+          <h4>I write to my digital friends every week.</h4>
+          <form
+            className="contact-form"
+            name="contact"
+            method="post"
+            netlify-honeypot="bot-field"
+            data-netlify="true"
+            action="/success"
+          >
+            {/* two hidden inputs for netlify form */}
+            <input type="hidden" name="bot-field" />
+            <input type="hidden" name="form-name" value="contact" />
+
+            <input
+              type="text"
+              name="nameInput"
+              placeholder="Your name"
+              className="form-control"
+            />
+            <input
+              type="email"
+              name="emailInput"
+              placeholder="Your email"
+              className="form-control"
+            />
+            <input
+              type="text"
+              name="messageInput"
+              placeholder="Your message"
+              className="form-control"
+            />
+            <button className="btn form-control submit-btn" type="submit">
+              Subscribe
+            </button>
+          </form>
+        </div>
+      </section>
+    </Layout>
   )
 }
 
