b4f2e068ed00fc8ebfd4aa5d94e8c3473b2acacd
ZoranUTF8 <janjiczoran23@gmail.com>
2022-01-03 17:53:06 +0900

    Re nder all posts to the posts page

--- src/pages/posts.js
+++ src/pages/posts.js
@@ -3,10 +3,43 @@ import Layout from '../components/Layout'
 import Hero from '../components/Hero'
 import { graphql } from 'gatsby'
 import Posts from '../components/Posts'
-const PostsPage = () => {
-return <h4>posts page</h4>
+import Post from '../components/Posts/Post'
+
+const PostsPage = ({ data }) => {
+  const {
+    allMdx: { nodes: posts },
+  } = data
+
+  return (
+    <Layout>
+      <Hero />
+      <Posts posts={posts} title={'all posts'} />
+    </Layout>
+  )
 }
 
-
+export const query = graphql`
+  {
+    allMdx(sort: { fields: frontmatter___date, order: DESC }) {
+      nodes {
+        frontmatter {
+          author
+          category
+          date(formatString: "MMMM,Do YYYY")
+          readTime
+          slug
+          title
+          image {
+            childImageSharp {
+              gatsbyImageData
+            }
+          }
+        }
+        id
+        excerpt
+      }
+    }
+  }
+`
 
 export default PostsPage
