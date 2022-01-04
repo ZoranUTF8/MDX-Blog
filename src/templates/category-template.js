8cb0c93229f327b64f4e1272077c136fcb397f5f
ZoranUTF8 <janjiczoran23@gmail.com>
2022-01-04 18:36:46 +0900

    Set up get category query in graphql

--- src/templates/category-template.js
+++ src/templates/category-template.js
@@ -4,14 +4,37 @@ import Hero from '../components/Hero'
 import Posts from '../components/Posts'
 import { graphql } from 'gatsby'
 
-const CategoryTemplate = ()=> {
-  
-  return (
-   <h2>category template</h2>
-  )
+const CategoryTemplate = (data) => {
+  console.log('====================================');
+  console.log(data);
+  console.log('====================================');
+  return <h2>category template</h2>
 }
 
-
-
+export const query = graphql`
+  query getCategories($category: String) {
+    allMdx(
+      sort: { fields: frontmatter___date, order: DESC }
+      filter: { frontmatter: { category: { eq: $category } } }
+    ) {
+      nodes {
+        frontmatter {
+          author
+          category
+          date(formatString: "MMMM,Do YYYY")
+          readTime
+          slug
+          title
+          image {
+            childImageSharp {
+              gatsbyImageData
+            }
+          }
+        }
+        excerpt
+      }
+    }
+  }
+`
 
 export default CategoryTemplate
